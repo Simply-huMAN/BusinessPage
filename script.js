15d9c6b67936deadd56521d09536d4f96ea8cb09
Maloth Aditya <76241066+Simply-huMAN@users.noreply.github.com>
2022-12-12 10:11:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,25 +7,28 @@ function toggleBtn() {
 }
 
 var faq = document.getElementsByClassName("faq-card");
-var ans = document.getElementsByClassName("answer");
 
 function answer_faq(num) {
   var Faq = faq[num];
-  var Ans = ans[num];
   Faq.classList.toggle("active-faq");
 }
 
 var opt = document.getElementById("opt");
 var option = document.getElementById("option");
+var opt1 = document.getElementById("opt1");
 val = false;
 function toggleOptions() {
   if (!val) {
     option.classList.remove("options");
     option.classList.add("active-options");
+    opt.classList.remove("fa-bars");
+    opt.classList.add("fa-xmark");
     val = true;
   } else {
     option.classList.remove("active-options");
     option.classList.add("options");
+    opt.classList.remove("fa-xmark");
+    opt.classList.add("fa-bars");
     val = false;
   }
 }
